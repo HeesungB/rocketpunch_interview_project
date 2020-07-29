70befb688ba456ca9bb3b71fc9e743fd38577345
HeesungB <hezo25@naver.com>
2020-07-29 13:49:15 +0900

    Change: Main Activity name to Message Activity

--- app/src/main/java/com/example/rocketpunch_interview/MessageActivity.kt
+++ app/src/main/java/com/example/rocketpunch_interview/MessageActivity.kt
@@ -3,9 +3,9 @@ package com.example.rocketpunch_interview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_message)
     }
 }
