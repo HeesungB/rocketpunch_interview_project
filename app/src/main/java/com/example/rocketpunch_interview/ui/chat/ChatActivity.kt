5352097f0eaabcb76c2e2ffb4fcd3244cc9b7965
HeesungB <hezo25@naver.com>
2020-07-30 17:11:36 +0900

    Add: ChatActivity.kt close button action

--- app/src/main/java/com/example/rocketpunch_interview/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/example/rocketpunch_interview/ui/chat/ChatActivity.kt
@@ -1,11 +1,10 @@
 package com.example.rocketpunch_interview.ui.chat
 
 import android.os.Bundle
-import android.util.Log
+import androidx.lifecycle.Observer
 import com.example.rocketpunch_interview.R
 import com.example.rocketpunch_interview.databinding.ActivityChatBinding
 import com.example.rocketpunch_interview.ui.base.BaseActivity
-import com.example.rocketpunch_interview.ui.message.MessageViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChatActivity: BaseActivity<ActivityChatBinding, ChatViewModel>() {
@@ -15,7 +14,9 @@ class ChatActivity: BaseActivity<ActivityChatBinding, ChatViewModel>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Log.d("zz",viewModel.selectedMessageChannel.value.toString())
+        viewModel.isPageClosed.observe(this, Observer {
+            finish()
+        })
     }
 
 
--- app/src/main/java/com/example/rocketpunch_interview/ui/chat/ChatViewModel.kt
+++ app/src/main/java/com/example/rocketpunch_interview/ui/chat/ChatViewModel.kt
@@ -1,9 +1,17 @@
 package com.example.rocketpunch_interview.ui.chat
 
+import androidx.lifecycle.LiveData
+import com.example.rocketpunch_interview.model.SingleLiveEvent
 import com.example.rocketpunch_interview.repository.MessageChannelRepository
 import com.example.rocketpunch_interview.ui.base.BaseViewModel
 
 class ChatViewModel(private val messageChannelRepository: MessageChannelRepository): BaseViewModel() {
     val selectedMessageChannel = messageChannelRepository.selectedMessageChannel
+    private val _isPageClosed = SingleLiveEvent<Any>()
 
+    val isPageClosed: LiveData<Any> get() = _isPageClosed
+
+    fun onClickBackButton() {
+        _isPageClosed.call()
+    }
 }
