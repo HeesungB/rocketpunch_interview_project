e6d5c0af3103e54e497f14db64b82a055625286d
HeesungB <hezo25@naver.com>
2020-07-31 11:17:34 +0900

    Add: Multidex setting

--- app/src/main/java/com/example/rocketpunch_interview/MessageApplication.kt
+++ app/src/main/java/com/example/rocketpunch_interview/MessageApplication.kt
@@ -1,12 +1,13 @@
 package com.example.rocketpunch_interview
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.example.rocketpunch_interview.di.repositoryModule
 import com.example.rocketpunch_interview.di.viewmodelAppModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class MessageApplication: Application() {
+class MessageApplication: MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
 
